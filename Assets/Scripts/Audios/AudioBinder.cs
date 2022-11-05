5196bd19eb80688db8de1d16b8f1e5796330264c
gongitsune <89068705+gongitsune@users.noreply.github.com>
2022-11-05 19:37:05 +0900

    回転を追加

--- Assets/Scripts/Audios/AudioBinder.cs
+++ Assets/Scripts/Audios/AudioBinder.cs
@@ -13,11 +13,12 @@ namespace Audios
         [SerializeField] private SpectrumAnalyzer spectrum;
         [SerializeField] private AudioLevelTracker levelTrackerHigh, levelTrackerMid, levelTrackerLow, levelTrackerBase;
         [SerializeField] private VisualEffect effect;
-
         private Vector4VfxProp _colorProp;
         private ConfigUI _conf;
-        private float _currentHue;
-        private FloatVfxProp _sizeProp, _amplitudeProp,_numOfParticle;
+        private float _currentHue, _lowElapsed;
+
+        private Rigidbody _effectRigid;
+        private FloatVfxProp _sizeProp, _amplitudeProp, _numOfParticle;
 
         private void Start()
         {
@@ -25,7 +26,7 @@ namespace Audios
             _amplitudeProp = new FloatVfxProp(effect, "Amplitude");
             _colorProp = new Vector4VfxProp(effect, "Color");
             _numOfParticle = new FloatVfxProp(effect, "SpawnRate");
-            
+
             _conf = ConfigUI.Instance;
             _conf.OnUpdate.Subscribe(_ =>
             {
@@ -36,6 +37,8 @@ namespace Audios
 
                 _numOfParticle.Value = (int)_conf.NumOfParticle;
             }).AddTo(this);
+
+            effect.TryGetComponent(out _effectRigid);
         }
 
         private void Update()
@@ -45,10 +48,32 @@ namespace Audios
             _sizeProp.Value = math.remap(0, 1, _conf.MinSize, _conf.MaxSize, baseLevel);
             _amplitudeProp.Value = math.remap(0, 1, _conf.MinAmplitude, _conf.MaxAmplitude, baseLevel);
 
+            if (levelTrackerHigh.normalizedLevel > 0.8f)
+                _effectRigid.AddTorque(0, baseLevel * 0.5f, 0, ForceMode.Impulse);
+            if (levelTrackerMid.normalizedLevel > 0.8f)
+                _effectRigid.AddTorque(baseLevel * 0.5f, 0, 0, ForceMode.Impulse);
+            if (levelTrackerLow.normalizedLevel > 0.8f)
+                _effectRigid.AddTorque(0, 0, baseLevel * 0.5f, ForceMode.Impulse);
+
             _currentHue += _conf.ColorSpeed * Time.deltaTime;
             if (_currentHue > 1) _currentHue = 0;
             var color = Color.HSVToRGB(_currentHue, _conf.Saturation, _conf.Brightness);
             _colorProp.Value = new Vector4(color.r, color.g, color.b, 1);
+
+            if (baseLevel < 0.1f)
+            {
+                _lowElapsed += Time.deltaTime;
+                if (_lowElapsed < 0.2f) return;
+                _lowElapsed = 0;
+                levelTrackerBase.ResetAutoGain();
+                levelTrackerHigh.ResetAutoGain();
+                levelTrackerMid.ResetAutoGain();
+                levelTrackerLow.ResetAutoGain();
+            }
+            else
+            {
+                _lowElapsed = 0;
+            }
         }
     }
 }
